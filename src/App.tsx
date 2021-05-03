92e90304a7fb6af06bbb12be529ce29ea5452236
Breno Lemos <brenomiros@gmail.com>
2021-05-03 20:57:37 -0300

    Add project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0%;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+
+  }
+`;
